fc125eb4df57478d49efea7e18dff667ab308465
Juan David Ospina Lizarazo <jdospinal2006@gmail.com>
2023-10-31 08:52:01 -0500

    ejercicio08.py

--- Python/ejercicio08.py
+++ Python/ejercicio08.py
@@ -1,22 +1,21 @@
-import math 
 # Función para calcular el área de un polígono regular
 def calcular_area_poligono(lado, lados):
     if lados == 3:
         # Triángulo
-        return (lado ** 2) * math.sqrt(3) / 4
+        return (lado ** 2) * (3 ** 0.5) / 4
     elif lados == 4:
         # Cuadrado
         return lado ** 2
     elif lados == 5:
         # Pentágono
-        apotema = lado / (2 * math.tan(math.pi / 5))
+        apotema = lado / 2.0 * (1 + (5 ** 0.5))
         return (5 * lado * apotema) / 2
     elif lados == 6:
         # Hexágono
-        return (3 * math.sqrt(3) * (lado ** 2)) / 2
+        return (3 * (3 ** 0.5) * (lado ** 2)) / 2
     else:
         # Caso por defecto: círculo
-        return math.pi * (lado ** 2)
+        return 3.1416 * (lado ** 2)
 
 # Solicitar al usuario la opción
 opcion = input("Elija una opción (1 para polígono, 2 para círculo): ")
@@ -30,7 +29,7 @@ if opcion == "1":
 elif opcion == "2":
     # Calcular área de un círculo
     radio = float(input("Ingresa el radio del círculo: "))
-    area = math.pi * (radio ** 2)
+    area = 3.1416 * (radio ** 2)
     print(f"El área del círculo con radio {radio} es: {area:.2f}")
 else:
     print("Opción no válida. Por favor, seleccione 1 o 2.")
