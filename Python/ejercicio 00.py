e47391421d572ed9d1f1945657cd29739ef2acc8
Juan David Ospina Lizarazo <jdospinal2006@gmail.com>
2023-11-07 08:18:38 -0500

    ejercicio 00.py

--- Python/ejercicio 00.py
+++ Python/ejercicio 00.py
@@ -1,16 +1,16 @@
-# Primer Programa que escribes en un nuevo lenguaje
+# Primer Programa que escribes en Python
 print("Hello, world!")
-print("**********************************************************************************************************************************")
-# Nombre De Los Participantes
+print("--------------------------------------------------------------------------------------------------------------------------------")
+# Nombre De lo del grupo
 print("Juan David Ospina, Juan Camilo Enciso Camacho")
 # Fecha
 print("Fecha:18 de Octubre Del 2023")
-# Ficha
+# Ficha de Diseño e Integracion de automatismos mecatronicos
 print(" Ficha:2749613")
 # Programa De Fomacion
 print(" Formacion:Diseño e Integracion de Automatismos Mecatronicos")
-# Edad
+# Edades
 print("  Edades:Camilo:18, Ospina:17")
-print("*********************************************************************************************************************************")
+print("--------------------------------------------------------------------------------------------------------------------------------")
 print("El texto  en pantalla sirve de interaccion con el usuario  de un programa")  
 print("Es prescindible sin embargo una adecuda interfaz permite,sin embargo una adecuada interfaz permite claridaden la ejecucion. ") 
