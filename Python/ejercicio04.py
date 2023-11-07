4767fedd730be482498304c47ee68335ed21a5e5
Juan David Ospina Lizarazo <jdospinal2006@gmail.com>
2023-11-07 08:25:42 -0500

    ejercicio04.py

--- Python/ejercicio04.py
+++ Python/ejercicio04.py
@@ -1,4 +1,4 @@
-v = int(input("ingresar el valor del voltage del circuito :"))
+V = int(input("ingresar el valor del voltage del circuito :"))
 r = int(input("ingresar el valor de la resistencia del circuito :"))
 res = V/r
 print ("Al conectar un resistor de R", r, "ohm" " " "a una fuente de V" " ", v, " " "voltage circulará una corriente de" " ", res, " Amperios" )
